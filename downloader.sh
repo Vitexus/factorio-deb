d4fa74dcbd6a76b19e81e6e8cc2088d4e8577cc3
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-08-25 10:29:14 +0200

    Downloaded version fix

--- downloader.sh
+++ downloader.sh
@@ -8,5 +8,5 @@ ARCHIVE="factorio_alpha_x64_${version}.tar.xz"
 
 CSRF=`curl -s -c ~/cookie.txt https://www.factorio.com/login | grep csrf_token | awk -F'"' '{print $8}'`
 curl -v -c ~/cookie.txt -b ~/cookie.txt  -H "X-CSRF-Token: ${CSRF}"  -X POST -F "csrf_token=${CSRF}" -F "username_or_email=${LOGIN}" -F "password=${PASSWD}" https://www.factorio.com/login
-curl -c ~/cookie.txt https://www.factorio.com/get-download/1.1.36/alpha/linux64 > ${ARCHIVE}
+curl -c ~/cookie.txt https://www.factorio.com/get-download/${version}/alpha/linux64 > ${ARCHIVE}
 
