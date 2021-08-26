aae364b80916d8bcb52f8b89db650b898da8fe1d
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-08-26 18:08:14 +0200

    VERSION fix

--- downloader.sh
+++ downloader.sh
@@ -3,10 +3,10 @@
 LOGIN=""
 PASSWD=""
 VERSION=`curl -s "https://api.github.com/repos/wube/factorio-data/tags" | jq -r '.[0].name'`
-ARCHIVE="factorio_alpha_x64_${version}.tar.xz"
+ARCHIVE="factorio_alpha_x64_${VERSION}.tar.xz"
 
 
 CSRF=`curl -s -c ~/cookie.txt https://www.factorio.com/login | grep csrf_token | awk -F'"' '{print $8}'`
 curl -v -c ~/cookie.txt -b ~/cookie.txt  -H "X-CSRF-Token: ${CSRF}"  -X POST -F "csrf_token=${CSRF}" -F "username_or_email=${LOGIN}" -F "password=${PASSWD}" https://www.factorio.com/login
-curl -c ~/cookie.txt https://www.factorio.com/get-download/${version}/alpha/linux64 > ${ARCHIVE}
+curl -c ~/cookie.txt https://www.factorio.com/get-download/${VERSION}/alpha/linux64 > ${ARCHIVE}
 
